6f41688f589b45c69ec2cb0979ed51bc88dee706
burner0621 <cutetiger0621@gmail.com>
2023-12-18 21:52:08 +0000

    backend fixed

--- controllers/profileContoller.py
+++ controllers/profileContoller.py
@@ -18,18 +18,17 @@ class ProfileController(Controller):
             try:
                 horse = dbManager.horseCol.getHorseById(id)
                 rlt  = []
-                print (len(horse['races']), ">>>>")
                 for race in horse['races']:
                     race = dict(race)
                     if 'barrier' in race['class'].lower(): continue
-                    race['_id'] = str(race['_id'])
+                    race['_id'] = str(race['_id']) if '_id' in race else ''
                     race['horse_foaling_date'] = race['horse_foaling_date'].strftime("%d/%m/%y") if 'horse_foaling_date' in race else ''
                     race['date'] = race['date'].strftime("%d/%m/%y") if 'date' in race else ''
                     race['home_date'] = race['home_date'].strftime("%d/%m/%y") if 'home_date' in race else ''
                     rlt.append (race)
-                print (len(rlt), "LLLLLLLLLLLL")
                 return rlt
-            except:
+            except Exception as e:
+                profileControllerLogger.error ("getRaceList() call failed.", exc_info=True)
                 return []
         
         if type == "trainer":
